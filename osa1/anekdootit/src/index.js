493d93cee8b30ec6a7ca140e572ee6050e26ae5e
Okko Hartikainen <okko.hartikainen@gmail.com>
2020-07-01 11:53:26 +0300

    Add part 1

---
+++ osa1/anekdootit/src/index.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = ({ title }) => <h1>{title}</h1>
+
+const Anecdote = ({ anecdote }) => <div>{anecdote}</div>
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const App = ({ anecdotes }) => {
+  const [selected, setSelected] = useState(random_anecdote_index)
+  const [votes, setVotes] = useState(new Uint8Array(anecdotes.length))
+
+  const random_anecdote = () => setSelected(random_anecdote_index)
+
+  const vote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    console.log(copy)
+    setVotes(copy)
+  }
+
+  return (
+    <div>
+      <Header title="Anecdote of the day" />
+      <Anecdote anecdote={anecdotes[selected]} />
+      <Button handleClick={vote} text="vote" />
+      <Button handleClick={random_anecdote} text="next anecdote" />
+      <Header title="Anecdote with most votes" />
+      <Anecdote anecdote={anecdotes[votes.indexOf(Math.max(...votes))]} />
+    </div>
+  )
+}
+
+const random_anecdote_index = () => Math.floor(Math.random()*anecdotes.length)
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)
---
+++ osa1/unicafe/src/index.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = ({ title }) => <h1>{title}</h1>
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+)
+
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad
+  const average = (good - bad)/all
+  const positive = 100*good/all
+
+  if (all === 0) {
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  }
+
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={all} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={positive + " %"} />
+      </tbody>
+    </table>
+  )
+}
+
+const App = (props) => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <Header title="give feedback" />
+      <Button
+        handleClick={() => setGood(good + 1)}
+        text='good'
+      />
+      <Button
+        handleClick={() => setNeutral(neutral + 1)}
+        text='neutral'
+      />
+      <Button
+        handleClick={() => setBad(bad + 1)}
+        text='bad'
+      />
+      <Header title="statistics" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
